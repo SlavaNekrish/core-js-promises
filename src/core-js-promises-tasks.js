83a761f66cb7b389c61ca9eea8721735eeed50f5
SlavaNekrish <vnekrish@mail.ru>
2024-04-14 23:59:18 +0300

    feat: implement 3rd and 4th promise tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -56,8 +56,13 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+async function getFirstResolvedPromiseResult(promises) {
+  try {
+    const result = await Promise.race(promises);
+    return result;
+  } catch (error) {
+    throw new Error('Promise rejected');
+  }
 }
 
 /**
@@ -79,8 +84,13 @@ function getFirstResolvedPromiseResult(/* promises */) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+async function getFirstPromiseResult(promises) {
+  try {
+    const result = await Promise.race(promises);
+    return result;
+  } catch (error) {
+    throw new Error('All promises were rejected');
+  }
 }
 
 /**
